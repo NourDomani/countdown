7ed4e83dac4ec70319e836bf230375f3e81ec2b9
Nour Domani <138979883+NourDomani@users.noreply.github.com>
2023-09-13 15:33:18 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-for (let index = 0; index <= 2; index++) {
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    basic.showNumber(3 - index)
-}
-music.playTone(392, music.beat(BeatFraction.Whole))
-basic.showString("go")
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(3 - index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("go")
+})
